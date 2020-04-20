7b2de39dbecf61f13d286fc20d30b2930c8a2000
sy <u_rongcha12@naver.com>
2020-04-20 18:23:05 +0900

    2020.04.20.18:23

--- src/main/java/shopping/crud/order/orderController.java
+++ src/main/java/shopping/crud/order/orderController.java
@@ -15,44 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class orderController {
 
-	@Autowired
-	BoardReplyDAO rdao;
 	
-	private static final Logger logger = LoggerFactory.getLogger(orderController.class);
-	
-	@RequestMapping(value="/boardreply.do", method = RequestMethod.GET)
-	public String reply_insert(BoardReplyDTO dto) {
-		rdao.dbInsert(dto);
-		return "redirect:/boardDetail.do?Gidx="+dto.getHobby_idx();
-	}//end
-	
-	@RequestMapping(value="/boardreply_list.do", method=RequestMethod.GET)
-	public ModelAndView reply_select(@RequestParam("idx") int idx) {
-		ModelAndView mav = new ModelAndView();
-		List<BoardReplyDTO> rLG = rdao.dbSelect(idx);
-		mav.addObject("rLG",rLG);
-		mav.setViewName("board_reply");
-		return mav;
-	}//end
-	
-	
-	@RequestMapping(value="/boardreply_delete.do")
-	public String reply_delete(@RequestParam("Ridx") int Ridx,@RequestParam("idx") int idx ) {
-	  rdao.dbDelete(Ridx);
-	  return "redirect:/boardDetail.do?Gidx="+idx;
-	}//end
-	
-	
-	@RequestMapping(value="/boardreply_edit.do")
-	public String reply_preEdit(@RequestParam("rhobby_idx") int rhobby_idx,@RequestParam("rwriter") String rwriter, @RequestParam("rmemo") String rmemo, @RequestParam("hobby_idx") int hobby_idx  ){
-		BoardReplyDTO rdto = new BoardReplyDTO();
-		rdto.setRhobby_idx(rhobby_idx);
-		rdto.setRwriter(rwriter);
-		rdto.setRmemo(rmemo);
-		rdto.setHobby_idx(hobby_idx);
-		rdao.dbUpdate(rdto);
-		return "redirect:/boardDetail.do?Gidx=" + rdto.getHobby_idx();
-	}//end
 	
 }//BoardReplyController class END
 
